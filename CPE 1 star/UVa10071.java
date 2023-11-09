4dc6c97db5e79fbeb52614bb079fae3766113325
AldanTai <142383968+AldanTai@users.noreply.github.com>
2023-11-10 01:05:20 +0800

    Update UVa10071.java

--- CPE 1 star/UVa10071.java
+++ CPE 1 star/UVa10071.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class UVa10071 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			try {
+				double muzzleVelocity = 0;
+				String line = sc.nextLine();
+				String arr[] = line.split(" ");
+				double speed = Double.parseDouble(arr[0]);
+				double time = Double.parseDouble(arr[1]);
+				double finalVelocity = (speed / time) * 2 * time;
+				double distance = (0 + finalVelocity) * 2 * time * (0.5);
+				System.out.println((int) distance);
+			} catch (Exception e) {
+				break;
+			}
+		}
+		sc.close();
 	}
 
 }
