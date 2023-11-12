6bd3fd117ee5d636fb94086e94bc992b7f2907bf
AldanTai <142383968+AldanTai@users.noreply.github.com>
2023-11-12 09:46:01 +0800

    Update UVa00100(The 3n + 1 problem).java

--- CPE 1 star/UVa00100(The 3n + 1 problem).java
+++ CPE 1 star/UVa00100(The 3n + 1 problem).java
@@ -5,15 +5,15 @@ public class UVa00100 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		while (true) {
+		while (sc.hasNext()) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			int min = Math.min(a, b);
 			int max = Math.max(a, b);
 
-			int c = 0; // 3n+1 所需要迴圈次數的最大值
+			int c = 0; 
 			for (int i = min; i <= max; i++) {
-				int temp = 1; // 3n+1 所需要迴圈次數
+				int temp = 1; 
 				for (int j = i; j != 1;) {
 					if (j % 2 == 0)
 						j = j / 2;
@@ -23,10 +23,11 @@ public class UVa00100 {
 				}
 
 				if (temp > c)
-					c = temp; // 判別此數的迴圈次數是否為最大值
+					c = temp; 
 			}
-			// Output
+			
 			System.out.println(a + " " + b + " " + c);
 		}
+		sc.close();
 	}
 }
