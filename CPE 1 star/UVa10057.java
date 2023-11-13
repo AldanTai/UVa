f7d4fa2da5e10125188693090a42eece563731f3
AldanTai <142383968+AldanTai@users.noreply.github.com>
2023-11-14 01:34:36 +0800

    Add files via upload

---
+++ CPE 1 star/UVa10057.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class UVa10057{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		
+		while(sc.hasNext()){
+			int cases=sc.nextInt();
+			
+			int arr[]=new int[cases];
+			for(int i=0;i<arr.length;i++){
+				arr[i]=sc.nextInt();
+			}
+			Arrays.sort(arr);
+			
+			
+			int mid=arr[(arr.length-1)/2];
+			int mid2=arr[(arr.length)/2];
+		
+			
+			int count=0;
+			for(int i=0;i<arr.length;i++){
+				if(arr[i]==mid || arr[i]==mid2) count++;
+			}
+			
+			
+			int D=mid2-mid+1;
+			
+			
+			System.out.println(mid+" "+count+" "+D);
+			
+		}
+	}
+}
