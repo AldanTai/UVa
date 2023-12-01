79065017329a6d2d68bc28f942ca80280713ba61
AldanTai <142383968+AldanTai@users.noreply.github.com>
2023-12-01 23:07:51 +0800

    Update UVa10019(Funny Encryption Method).java

--- CPE 1 star/UVa10019(Funny Encryption Method).java
+++ CPE 1 star/UVa10019(Funny Encryption Method).java
@@ -5,18 +5,15 @@ public class UVa10019 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int quantity = sc.nextInt();
-		while (true) {
-			try {
-				int N = sc.nextInt();
-				String X1 = Integer.toBinaryString(N);
-				long b1 = X1.chars().filter(ch -> ch == '1').count();
-				String A = Integer.valueOf(Integer.toString(N), 16).toString();
-				String X2 = Integer.toBinaryString(Integer.parseInt(A));
-				long b2 = X2.chars().filter(ch -> ch == '1').count();
-				System.out.println(b1 + " " + b2);
-			} catch (Exception e) {
-				break;
-			}
+		while (sc.hasNext()) {
+
+			int N = sc.nextInt();
+			String X1 = Integer.toBinaryString(N);
+			long b1 = X1.chars().filter(ch -> ch == '1').count();
+			String A = Integer.valueOf(Integer.toString(N), 16).toString();
+			String X2 = Integer.toBinaryString(Integer.parseInt(A));
+			long b2 = X2.chars().filter(ch -> ch == '1').count();
+			System.out.println(b1 + " " + b2);
 		}
 
 	}
